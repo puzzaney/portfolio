cdc4e7782a0285df526f20d30dbe0f602f25902c
PuzZan Karki <poojankarki12@gmail.com>
2023-08-23 19:54:12 +0545

    Removed Unnecessary Code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="nav">
       <div className="nav__title">PuzZan;</div>
-      <div className="nav__items"></div>
+      <ul className="nav__items">
+        <li>
+          <a href="#" className="nav__link">
+            Home
+          </a>
+          <a href="#" className="nav__link">
+            About Me
+          </a>
+          <a href="#" className="nav__link">
+            Projects
+          </a>
+          <a href="#" className="nav__link">
+            Contact Me
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
