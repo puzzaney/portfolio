6dea10218a6ae0ae10879c150013f58947255661
PuzZan Karki <poojankarki12@gmail.com>
2023-08-23 19:40:53 +0545

    Created Navbar.jsx and base.scss

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="nav__title">PuzZan;</div>
+      <div className="nav__items"></div>
+    </nav>
+  );
+}
