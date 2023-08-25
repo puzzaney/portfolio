b095b1fbb8551745d167b2547b929f72f78792eb
PuzZan Karki <poojankarki12@gmail.com>
2023-08-25 11:26:25 +0545

    MOved Icon container to different component

---
+++ src/components/SkillsIconContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Icon from './Icon';
+
+export default function ({ title1, icon1, title2, icon2, title3, icon3 }) {
+  return (
+    <div className="about__icon-container">
+      <Icon title={title1}>{icon1}</Icon>
+
+      <Icon title="title2">{icon2}</Icon>
+
+      <Icon title="title3">{icon3}</Icon>
+    </div>
+  );
+}
